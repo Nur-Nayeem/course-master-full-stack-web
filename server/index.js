8185d6ac8d6d1ce4a9b4e6ebe844af1f39aabf8c
nur-nayeem <nurnayem768@gmail.com>
2025-12-01 22:47:31 +0600

    initalize server, create user schema and make login/register apis with jwt

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import authRoutes from "./routes/auth.js";
+const port = process.env.PORT;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// app.get((req, res) => {
+//   console.log("server is running");
+// });
+
+app.use("/api/auth", authRoutes);
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    app.listen(port, () =>
+      console.log(`Server running on http://localhost:${port}`)
+    );
+  })
+  .catch((err) => console.log(err));
---
+++ server/routes/auth.js
@@ -0,0 +1,57 @@
+import express from "express";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { auth } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/me", auth, (req, res) => {
+  res.json({ message: "Protected", userId: req.user.id });
+});
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const exists = await User.findOne({ email });
+    if (exists)
+      return res.status(400).json({ message: "Email already registered" });
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await User.create({ name, email, password: hashed });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.json({ message: "Registered", token, user });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "User not found" });
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) return res.status(400).json({ message: "Wrong password" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.json({ message: "Logged in", token, user });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+export default router;
