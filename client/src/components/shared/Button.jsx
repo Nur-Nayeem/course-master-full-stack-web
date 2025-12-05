9715ee12fb62c0b5ad31b910e2fad287cc56bfa3
nur-nayeem <nurnayem768@gmail.com>
2025-12-05 22:23:58 +0600

    delete unnecessary folder

--- client/src/components/shared/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Button = ({ children, py = 2, px = 3 }) => {
-  return (
-    <button className={`btn-primary px-${px} py-${py} `}>{children}</button>
-  );
-};
-
-export default Button;
